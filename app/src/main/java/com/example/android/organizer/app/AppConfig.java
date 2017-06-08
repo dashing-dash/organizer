de98f5dae42443fc74c6731bfeeee7703388af65
dashing-dash <pal21019@gmail.com>
2017-06-08 20:27:28 +0530

    First Commit

---
+++ app/src/main/java/com/example/android/organizer/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.android.organizer.app;
+
+/**
+ * Created by Pallav on 6/8/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.58/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.43.58/android_login_api/register.php";
+}
